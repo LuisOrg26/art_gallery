655c96e74bc841d04842c505a33488971fed8d37
LuisOrg26 <luisjaorg26mail.com>
2023-11-26 01:23:01 -0600

    Improving

--- art_gallery1.2/src/app/welcome/welcome.component.ts
+++ art_gallery1.2/src/app/welcome/welcome.component.ts
@@ -1,27 +1,30 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api.service';
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.css'
+  styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
-
   title = 'Art Gallery';
-  query: string = '';
   content: any;
   paint: string = '';
 
   constructor(private service: ApiService) {}
 
-  searchApi() {
-    this.service.getArt(this.query).subscribe(
+  searchApi(query: string) {
+    this.service.getArt(query).subscribe(
       (data) => {
         this.content = data;
+        this.paint = this.content.data[0].title;
+        // You can do additional processing or UI updates here after the API call completes.
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+        // Handle errors if needed
       }
-
     );
-    this.paint = this.content.data[0].title;
   }
-
 }
+
