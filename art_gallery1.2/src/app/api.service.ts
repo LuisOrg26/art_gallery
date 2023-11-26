9d7f191904dc0957a935afe071de7acc104f2c42
LuisOrg26 <luisjaorg26mail.com>
2023-11-26 12:39:18 -0600

    New API and a proxy Server added

--- art_gallery1.2/src/app/api.service.ts
+++ art_gallery1.2/src/app/api.service.ts
@@ -5,13 +5,9 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class ApiService {
-  private arturl = 'https://www.wikiart.org/en/api/2/PaintingSearch?term=';
+  private arturl = 'http://localhost:3000/api/en/api/2/PaintingSearch?term=';
   private sessionurl ='https://www.wikiart.org/es/Api/2/login?accessCode=bd31b0255f72424c&secretCode=9bee2f088562cfa3'
-  private session:any;
-  private body = {
-    'accessCode': 'bd31b0255f72424c',
-    'secretCode': '9bee2f088562cfa3'
-  }
+  public sessionkey:any;
   private headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.5',
@@ -19,11 +15,13 @@ export class ApiService {
   };
   
   constructor(private http: HttpClient) {
-    this.session = this.http.post(this.sessionurl,this.body);
   }
 
-  getArt(search: any,imageFormat='Large') {
+  getArt(search: any) {
     const head = this.headers
-    return this.http.get(this.arturl +search+'&imageFormat='+imageFormat,{headers:head});
+    return this.http.get(this.arturl +search);
+  }
+  getSession(){
+    return this.sessionkey = this.http.get(this.sessionurl);
   }
 }
--- art_gallery1.2/src/app/welcome/welcome.component.ts
+++ art_gallery1.2/src/app/welcome/welcome.component.ts
@@ -15,16 +15,24 @@ export class WelcomeComponent {
   image: string=''
   content_second:any;
   imageFormat:string ='Large';
+  session_id:string=""
   constructor(private service: ApiService) {}
 
   searchApi(query: string) {
-    this.service.getArt(query,this.imageFormat).subscribe(
+    this.service.getArt(query).subscribe(
       (data) => {
         this.content = data;
-        this.paint = this.content.data[0].title;
-        this.id = this.content.data[0].id;
-        this.artist = this.content.data[0].artistname;
-        this.image = this.content.data[0].image;
+        if (this.content.data){
+          this.paint = this.content.data[0].title;
+          this.id = this.content.data[0].id;
+          this.artist = this.content.data[0].artistName;
+          this.image = this.content.data[0].image;
+          this.content = null;
+        }else{
+          this.paint = "No artwork found";
+          this.artist = "Choose other words";
+          this.image = "";
+        }
       }   
     );
   }
